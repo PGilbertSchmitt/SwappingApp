6727e22a9d306fb2f5ce779ec649433375e4b0b3
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-14 14:33:09 -0700

    Create User migration

---
+++ db/migrate/20170314210314_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :fname
+      t.string :lname
+      t.string :address
+      t.string :phone_number
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
