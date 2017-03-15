a43810db479c669ad0963225a0c7e1516411a9db
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-15 10:09:45 -0700

    Add todo

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const App = ({ children }) => (
   <div>
+    {/* TODO: Build header, footer, and categories and render here*/}
     <header>This is the header</header>
     {children}
   </div>
