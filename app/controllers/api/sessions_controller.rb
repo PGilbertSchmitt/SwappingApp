a538f23f550eaecd5fadc5c5313ca826377d65b5
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-15 14:11:48 -0700

    Begin initial styling

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,15 +1,18 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.credentials( 
+    @user = User.find_by_credentials( 
       params[:user][:email],
       params[:user][:password]
     )
+    p params[:user][:email]
+    p params[:user][:password]
 
     if @user
       login(@user)
       render 'api/users/show'
     else
       render json: ["Invalid credentials"], status: 401
+      # render json: @user.errors.fullmessages, status: 401
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
     SecureRandom::urlsafe_base64(16)
   end
 
-  def self.credentials(email, password)
+  def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
     user && user.is_password?(password) ? user : nil
   end
