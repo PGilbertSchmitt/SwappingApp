9d6f6d35718c2fc8c9886879722e09eec2af61e0
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-14 17:36:23 -0700

    Change entry file name

---
+++ frontend/swappin_app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import configureStore from './store/store';
+import Root from './components/root';
+import * as Auth from './util/auth_util_api';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  // const store = configureStore();
+  ReactDOM.render(<Root />, root);
+});
