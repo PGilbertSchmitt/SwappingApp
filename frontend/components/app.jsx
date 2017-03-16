fa1d91396d9aabe4241699e4f55224a88880bdf6
Gilbert <PGilbertSchmitt@gmail.com>
2017-03-16 11:19:20 -0700

    Remove todo, items will be in seperate component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 const App = ({ children }) => (
   <div>
-    {/* TODO: Build header, footer, and categories and render here*/}
-    {/*<header className="header-bar u-full-width">
-      <a>This is the header</a>
-    </header>*/}
     {children}
   </div>
 );
